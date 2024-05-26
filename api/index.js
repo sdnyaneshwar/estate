36e035b79f37f57d532326ca3985a55d20ed4459
dnyaneshwar suwarnkar <sdnyaneshwar>
2024-05-26 11:31:03 +0530

    create server and controller

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from 'express'
-
+import mongoose, { connect } from 'mongoose';
+import dotenv from 'dotenv'
+import userRouter from './routes/user.route.js';
+dotenv.config();
 const app= express()
-
+mongoose.connect(process.env.MONGO_ID).then(()=>{
+    console.log("database is connected");
+}).catch((error)=>{
+    console.log(error);
+})
 app.listen(3000,()=>{
     console.log("server is runnin on port 3000");
 })
+
+
+app.use('/api/user',userRouter)
